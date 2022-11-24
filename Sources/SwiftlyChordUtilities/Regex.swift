be57ad5d6713213089bc2f90e0903f222a6702df
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2022-11-24 12:35:31 +0100

    Snapshot

---
+++ Sources/SwiftlyChordUtilities/Regex.swift
@@ -0,0 +1,56 @@
+//
+//  ChordFinder+Regex.swift
+//  Chords Database
+//
+//  Created by Nick Berendsen on 11/11/2022.
+//
+
+import Foundation
+import RegexBuilder
+
+/// The regex for a `chord` string
+///
+/// It will parse the chord to find the `root` and optional `quality`
+///
+///     /// ## Examples
+///
+///     Am -> root: Am, quality: nil
+///     Dsus4 -> root: D, quality: sus4
+///
+let chordRegex = Regex {
+    /// The root
+    Capture {
+        OneOrMore {
+            CharacterClass(
+                .anyOf("CDEFGABb#")
+            )
+        }
+    }
+    /// The optional quality
+    Optionally {
+        Capture {
+            OneOrMore(.any)
+        }
+    }
+}
+
+let inversionRegex = Regex {
+    "/"
+    Capture {
+        OneOrMore(("0"..."9"))
+    }
+    Capture {
+        ZeroOrMore(.any)
+    }
+}
+
+let slashRegex = Regex {
+    /// The slash
+    Capture {
+        OneOrMore("/")
+    }
+    /// The following chord
+    Capture {
+        OneOrMore(.any)
+    }
+}
---
+++ Sources/SwiftlyChordUtilities/Utils.swift
@@ -0,0 +1,27 @@
+//
+//  ChordFinder+Utils.swift
+//  Chords Database
+//
+//  Created by Nick Berendsen on 12/11/2022.
+//
+
+import Foundation
+import SwiftyChords
+
+/// Get index value of a note
+func noteToValue(note: Chords.Root) -> Int {
+    guard let value = noterValueDict[note] else {
+        return 0
+    }
+    return value
+}
+
+/// Return note by index in a scale
+func valueToNote(value: Int, scale: Chords.Root = .c) -> Chords.Key {
+    let value = value < 0 ? (12 + value) : (value % 12)
+    //guard let value = scaleValueDict[Chords.Root(rawValue: scale) ?? .c]?[value] else {
+    guard let value = scaleValueDict[scale]?[value] else {
+        return .c
+    }
+    return value
+}
